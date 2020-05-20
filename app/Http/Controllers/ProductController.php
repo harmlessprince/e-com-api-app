8228a8cca048312f95d611433471c812d5798673
harmlessprince <realolamilekan@gmail.com>
2020-05-20 06:15:17 +0100

    Product Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductCollection;
 use App\Http\Resources\ProductResource;
 use App\Product;
@@ -9,6 +10,11 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -36,9 +42,44 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
+        $product = Product::create([
+            "name"  => $request->name,
+            "detail"  => $request->description,
+            "stock"  => $request->stock,
+            "price"  => $request->price,
+            "discount"  => $request->discount,
+        ]);
+
+        if ($product) {
+            return response([
+                "data"  => new ProductResource($product),
+            ], 201);
+        }else{
+            return response([
+                "error"  => "Product not created",
+            ],200);
+        }
+
+        // "name": [
+        //     "The name field is required."
+        // ],
+        // "detail": [
+        //     "The detail field is required."
+        // ],
+        // "stock": [
+        //     "The stock field is required."
+        // ],
+        // "price": [
+        //     "The price field is required."
+        // ],
+        // "discount": [
+        //     "The discount field is required."
+        // ]
         //
+        // return $request->all();
+        // $product = Produc
     }
 
     /**
@@ -73,6 +114,18 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
         //
+        $request['detail'] = $request->description ;
+
+        unset($request['description']);
+
+        $product->update($request->all());
+
+        return response([
+            "data"  => new ProductResource($product),
+        ], 201);
+    
+
+
     }
 
     /**
